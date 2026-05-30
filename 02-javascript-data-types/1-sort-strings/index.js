d1eff2024863dfd0f056761599d3c424752cfe91
Ivan Grigor <vanya.grigorevskiy@gmail.com>
2026-05-30 13:53:57 +0300

    02-javascript-data-types done

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,12 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+    const collator = new Intl.Collator(['RU', 'EN'], {
+        sensitivity: 'variant',
+        caseFirst: 'upper'
+    });
+    return [...arr].sort((a, b) => {
+        if (param === 'asc') return collator.compare(a, b)
+        else if (param === 'desc') return collator.compare(b, a)
+    })
 }
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,7 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+    const newObj = {}
+    Object.entries(obj).forEach((el) => !fields.includes(el[0]) ? newObj[el[0]] = el[1] : null)
+    return newObj
 };
